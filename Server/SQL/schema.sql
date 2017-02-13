5ae076f7d550158816d97b4aa269bc7e6791cf2a
Clayton Passmore <ctpassmore@gmail.com>
2017-02-13 16:28:33 -0500

    preliminary schema
    typo fix
    
    addressing matts comments
    
    moved things around, now usable
    
    add needreview table
    
    remove needs review from card

---
+++ Server/SQL/schema.sql
@@ -0,0 +1,74 @@
+CREATE TABLE IF NOT EXISTS User
+(
+    uuid VARCHAR(36) NOT NULL PRIMARY KEY,
+    access_token VARCHAR(36) NOT NULL,
+    name VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS Deck
+(
+    uuid VARCHAR(36) NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    owner VARCHAR(36) NOT NULL,
+    rating FLOAT DEFAULT NULL,
+    num_ratings INTEGER NOT NULL DEFAULT 0,
+    public BOOLEAN NOT NULL DEFAULT FALSE,
+    deleted BOOLEAN NOT NULL DEFAULT FALSE,
+    version INTEGER NOT NULL DEFAULT 0,
+    share_code VARCHAR(8) UNIQUE,
+
+    FOREIGN KEY (owner) REFERENCES User(uuid)
+);
+
+
+CREATE TABLE IF NOT EXISTS Card
+(
+    uuid INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    deck_id VARCHAR(36) NOT NULL,
+    front VARCHAR(255) NOT NULL,
+    back VARCHAR(255) NOT NULL,
+    position INTEGER NOT NULL,
+
+    FOREIGN KEY (deck_id) REFERENCES Deck(uuid) ON DELETE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS NeedReview
+(
+    card_id INTEGER NOT NULL,
+    user_id VARCHAR(36) NOT NULL,
+
+    PRIMARY KEY (card_id, user_id),
+    FOREIGN KEY (card_id) REFERENCES Card(uuid) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES User(uuid)
+);
+
+
+CREATE TABLE IF NOT EXISTS Library
+(
+    user_id VARCHAR(36) NOT NULL,
+    deck_id VARCHAR(36) NOT NULL,
+
+    PRIMARY KEY (user_id, deck_id),
+    FOREIGN KEY (user_id) REFERENCES User(uuid),
+    FOREIGN KEY (deck_id) REFERENCES Deck(uuid)
+);
+
+
+CREATE TABLE IF NOT EXISTS Tag
+(
+    uuid INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    tag VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS DeckTag
+(
+    tag_id INTEGER NOT NULL,
+    deck_id VARCHAR(36) NOT NULL,
+
+    PRIMARY KEY (tag_id, deck_id),
+    FOREIGN KEY (tag_id) REFERENCES Tag(uuid),
+    FOREIGN KEY (deck_id) REFERENCES Deck(uuid)
+);
